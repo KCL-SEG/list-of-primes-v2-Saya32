afdd504a62cad3143e16570c34432dafede0904c
Sayaka Bhandari <sayakabhandari@gmail.com>
2022-10-13 17:36:55 +0100

    first trial to test

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    while True:
+        try:
+            for i in range(2, number_of_primes + 1):
+                for j in range(2, int(i ** 0.5) + 1):
+                    if i%j == 0:
+                        break
+                    else:
+                        primes.append(i)
+            print (list)
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+        print (list)
