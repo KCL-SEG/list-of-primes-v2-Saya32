e674417553c44fb84fc818f83853b18d14a6db69
Sayaka Bhandari <sayakabhandari@gmail.com>
2022-10-21 23:50:59 +0100

    Appending the prime number

--- primes.py
+++ primes.py
@@ -5,12 +5,15 @@ def primes(number_of_primes):
     list = []
     while True:
         try:
-            for i in range(2, number_of_primes + 1):
-                for j in range(2, int(i ** 0.5) + 1):
-                    if i%j == 0:
-                        break
-                    else:
-                        primes.append(i)
-            return list
+                #append all the prime number  till number_of_primes
+            i = 2        # first prime number
+            while len(list) != number_of_primes:    # while list is not full
+                for j in range(2, i):   # check if i is prime
+                    if i % j == 0:      # if i is not prime
+                        break       # break the loop
+                else:       # if i is prime
+                    list.append(i)    # append i to list
+                i+=1    # increment i
+            return list     # return list of primes
         except ValueError:
             print("Oops!  That was no valid number.  Try again...")
