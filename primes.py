79f69c504c4c9d5e0df89120d39b91a7e45b36e1
Saya32 <sayakabhandari@gmail.com>
2022-12-03 23:23:49 +0000

    New changes

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    try:
+            i = 2        # first prime number
+            while len(list) != number_of_primes:    # while list is not full
+                for j in range(2, i):   # check if i is prime
+                    if i % j == 0:      # if i is not prime
+                        break       # break the loop
+                else:       # if i is prime
+                    list.append(i)    # append i to list
+                i+=1    # increment i
+            return list     # return list of primes
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
